89f5973dfa12e0042b9bd7f8011bff0a807b1541
Lathif Ramadhan <168287240+LatiefDataVisionary@users.noreply.github.com>
2024-12-29 21:08:19 +0700

    Add files via upload

---
+++ python_ke_biner.py
@@ -0,0 +1,20 @@
+import dis
+
+def kode():
+    num1 = float(input("Masukkan angka pertama: "))
+    num2 = float(input("Masukkan angka kedua: "))
+    hasil_penjumlahan = num1 + num2
+    if hasil_penjumlahan == 0 and 5:
+        print("hehehehe!!!")
+    elif hasil_penjumlahan == 2:
+        print("lolololo")
+    else:
+        pass
+    print(hasil_penjumlahan)
+
+    hasil_pembagian = int(num1) / int(num2)
+    if hasil_pembagian == 0:
+        print("jangan nol lah!!!!")
+    print(hasil_pembagian)
+
+dis.dis(kode)
